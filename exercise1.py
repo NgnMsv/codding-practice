f4826021824151e230e349b797cfb69a79d9873b
NgnMsv <ngn.m2000@gmail.com>
2023-02-16 23:09:19 +0330

    exercise1 solved

--- exercise1.py
+++ exercise1.py
@@ -1,1 +1,36 @@
-#hello word
+def main():
+    numbers = []
+    for i in range(10):
+        num = int(input())
+        numbers.append(num)
+    primelist = primes(max(numbers))
+    finalArr = []
+    mainEl = 0
+    maxLen = 0
+    for n in numbers:
+        tempArr = []
+        sum = 0
+        for p in primelist:
+            if n%p == 0:
+                tempArr.append(p)
+                sum += 1
+        if sum > maxLen:
+            if n > mainEl:
+                maxlen = sum
+                mainEl = n
+    print(mainEl, maxlen)
+    
+    finalArr
+
+def primes(max):
+    primeList = []
+    for i in range(2 , max):
+        isprime = True
+        for j in range(2 , i):
+            if (i%j) == 0:
+                isprime = False 
+        if isprime == True:
+            primeList.append(i)
+    return primeList
+if __name__=="__main__":
+    main()
